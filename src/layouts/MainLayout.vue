083a40df8aef8707b369276e2c8b7068a8a0adad
Nasir <nasirlawal001@gmail.com>
2021-07-06 19:34:45 +0100

    add prototype to the component

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -10,22 +10,19 @@
           aria-label="Menu"
           @click="toggleLeftDrawer"
         />
-
         <q-toolbar-title class="tw-text-lg sm:tw-text-xl">
           Attendance
         </q-toolbar-title>
-
         <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
-
     <q-drawer
       v-model="leftDrawerOpen"
       show-if-above
       bordered
       class="bg-grey-1"
     >
-      <div class="tw-mt-4 tw-text-lg tw-text-gray-700 tw-text-center">Attendance System</div>
+      <div class="tw-mt-4 tw-text-lg tw-text-gray-500 tw-text-center">Attendance System</div>
       <q-list class="q-pt-md">
         <q-expansion-item
           class="tw-text-sm sm:tw-text-base tw-text-gray-600"
@@ -40,6 +37,7 @@
             v-for="child in link.children"
             :key="child.title"
             :inset-level="0.5"
+            :to="child.to"
             active-class="text-primary"
           >
             <q-item-section avatar>
@@ -48,20 +46,17 @@
             <q-item-section>{{ child.title }}</q-item-section>
           </q-item>
         </q-expansion-item>
-
         <q-item
           v-ripple
           clickable
         >
-          <q-item-section avatar>
+          <q-item-section class="tw-text-gray-600" avatar>
             <q-icon name="logout" />
           </q-item-section>
-
           <q-item-section class="tw-text-sm sm:tw-text-base tw-text-gray-600">Logout</q-item-section>
         </q-item>
       </q-list>
-    </q-drawer>
-
+    </q-drawer>  
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -69,8 +64,9 @@
 </template>
 
 <script>
-
 import { defineComponent, ref } from 'vue'
+import { api } from 'boot/axios';
+import { useQuasar } from 'quasar';
 
 const linksData = [
   {
@@ -85,7 +81,12 @@ const linksData = [
       {
         title: 'Visitors List',
         icon: 'people_outline',
-        to: { name: 'visitor' }
+        to: { name: 'visitors' }
+      },
+      {
+        title: 'Search Visitor',
+        icon: 'search',
+        to: { name: 'visitors' }
       }
     ]
   },
@@ -95,14 +96,32 @@ export default defineComponent({
   name: 'MainLayout',
 
   setup () {
+    const $q = useQuasar()
     const leftDrawerOpen = ref(false);
     
+    const loadData = () => {
+      alert('yesssss')
+      api.get(`/api/attendance`)
+        .then((response) => {
+          console.log(response.data)
+        })
+        .catch(() => {
+          $q.notify({
+            color: 'negative',
+            position: 'top',
+            message: 'Failed to load visitors',
+            icon: 'report_problem'
+          })
+        })
+    }
+
     return {
       links: linksData,
       leftDrawerOpen,
       toggleLeftDrawer () {
         leftDrawerOpen.value = !leftDrawerOpen.value
-      }
+      },
+      loadData
     }
   }
 })
