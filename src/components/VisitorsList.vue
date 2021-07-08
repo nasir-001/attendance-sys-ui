d16c201ff71489dafd45c9750c335bc8d80adf76
Nasir <nasirlawal001@gmail.com>
2021-07-08 11:03:58 +0100

    finish visitors list component

--- src/components/VisitorsList.vue
+++ src/components/VisitorsList.vue
@@ -1,28 +1,40 @@
 <template>
-  <div class="q-pa-md">
+  <div class="q-pa-md q-mt-lg xl:tw-w-5/6 tw-mx-auto">
     <q-table
-      class="text-grey-8"
       title="Expected Visitors"
       :rows="rows"
       :columns="columns"
+      :visibleColumns="visibleColumns"
       row-key="name"
       binary-state-sort
     >
       <template v-slot:body="props">
         <q-tr :props="props">
           <q-td key="image" :props="props">
-            <q-img :src="image" />
+            <q-img class="tw-w-12 tw-h-12 md:tw-w-16 md:tw-h-16 tw-object-cover tw-rounded-lg hover:tw-shadow-md" :src="props.row.image" />
           </q-td>
-          <q-td key="title" :props="props">{{ props.title }}</q-td>
-          <q-td key="first_name" :props="props">{{ props.first_name }}</q-td>
-          <q-td key="last_name" :props="props">{{ props.last_name }}</q-td>
-          <q-td key="phone" :props="props">{{ props.phone }}</q-td>
-          <q-td key="email" :props="props">{{ props.email }}</q-td>
+          <q-td key="title" :props="props">
+            {{ props.row.title }}
+          </q-td>
+          <q-td key="first_name" :props="props">
+            {{ props.row.first_name }}
+          </q-td>
+          <q-td key="last_name" :props="props">
+            {{ props.row.last_name }}
+          </q-td>
+          <q-td key="phone" :props="props">
+            {{ props.row.phone }}
+          </q-td>
+          <q-td key="email" :props="props">
+            {{ props.row.email }}
+          </q-td>
           <q-td key="status" :props="props">
-            <q-badge class="tw-py-1 tw-px-2 tw-right-0" color="positive" label="Approved" />
+            <q-badge class="tw-py-1 tw-px-2 tw-right-0" v-if="props.row.status === 'admitted'" color="positive" label="Approved" />
+            <q-badge class="tw-py-1 tw-px-2 tw-right-0" v-if="props.row.status === 'cancel'" color="negative" label="Cancelled" />
+            <q-badge class="tw-py-1 tw-px-2 tw-right-0" v-if="props.row.status == 'pending' " color="warning" label="Pending" /> 
           </q-td>
-          <q-td key="view" :props="props">
-            <q-btn size="10px" color="primary" label="View" icon-right="visibility" />
+          <q-td>
+            VIEW            
           </q-td>
         </q-tr>
       </template>
@@ -31,61 +43,126 @@
 </template>
 
 <script>
-import { defineComponent, ref} from 'vue';
+import { computed, defineComponent, ref} from 'vue';
+import { useAttendanceService } from '../composables/attendanceService';
+import { useQuasar } from 'quasar';
 
 const columns = [
   {
-    name: 'name',
+    name: 'image',
     required: true,
-    label: 'Image',
+    label: 'IMAGE',
     align: 'left',
     field: row => row.image,
   },
-  { name: 'title', label: 'Title', field: 'title' },
-  { name: 'first_name', label: 'First Name', field: 'first_name' },
-  { name: 'last_name', label: 'Last Name', field: 'last_name' },
-  { name: 'phone', label: 'Phone', field: 'phone' },
-  { name: 'email', label: 'Email', field: 'email' },
-  { name: 'status', label: 'Status', field: 'status' },
-  { name: 'view', label: 'View', field: 'view' }
+  { name: 'title', label: 'TITLE', align: 'left', field: 'title' },
+  { name: 'first_name', label: 'FIRST NAME', align: 'left', field: 'first_name' },
+  { name: 'last_name', label: 'LAST NAME', align: 'left', field: 'last_name' },
+  { name: 'phone', label: 'PHONE', align: 'left', field: 'phone' },
+  { name: 'email', label: 'EMAIL', align: 'left', field: 'email' },
+  { name: 'status', label: 'STATUS', align: 'left', field: 'status' },
+  { name: 'view', label: 'VIEW', align: 'left', field: 'view' }
+]
+
+const rows = [
+  {
+    id: "1",    
+    title: "Senator",
+    first_name: "Musa",    
+    last_name: "Muhammad",    
+    email: "musamohd@gmail.com",
+    phone: "08109342356",
+    gender: "male",
+    image: "/img/pexels-public-domain-pictures-42059.jpg",
+    status: 'admitted'
+  },
+  {
+    id: "2",    
+    title: "Mr",
+    first_name: "Nasir",    
+    last_name: "Lawal",    
+    email: "nla@gmail.com",
+    phone: "08109342356",
+    gender: "male",
+    image: "/img/pexels-polina-tankilevitch-4518599.jpg",
+    status: "pending"
+  },
+  {
+    id: "3",    
+    title: "Governor",
+    first_name: "Ibrahim",    
+    last_name: "Muazu",    
+    email: "ibrahimmuaz@gmail.com",
+    phone: "08109342356",
+    gender: "male",
+    image: "/img/pexels-naman-nayar-922764.jpg",
+    status: 'admitted'
+  },
+  {
+    id: "4",    
+    title: "Chairman",
+    first_name: "Isah",    
+    last_name: "Ahmad",    
+    email: "isah2312@gmail.com",
+    phone: "08109342356",
+    gender: "male",
+    image: "/img/pexels-ready-made-3850652.jpg",
+    status: 'cancel'
+  },
+  {
+    id: "5",    
+    title: "Rep",
+    first_name: "Muhammadu",    
+    last_name: "Junaid",    
+    email: "mohdjunnaid@gmail.com",
+    phone: "08109342356",
+    gender: "male",
+    image: "/img/pexels-pixabay-161559.jpg",
+    status: 'cancel'
+  },
+  {
+    id: "6",    
+    title: "Mrs",
+    first_name: "Farida",    
+    last_name: "Aliyu",    
+    email: "farido@gmail.com",
+    phone: "08109342356",
+    gender: "female",
+    image: "/img/pexels-wendy-wei-1656666.jpg",
+    status: "pending"
+  },
+  {
+    id: "7",    
+    title: "Developer",
+    first_name: "Nasir",    
+    last_name: "Ibrahim",    
+    email: "nasirib@gmail.com",
+    phone: "08109342356",
+    gender: "male",
+    image: "/img/pexels-julia-volk-5273044.jpg",
+    status: 'admitted',
+  }
 ]
 
 export default defineComponent({
   name: 'VisitorsList',
-  props: {
-    id: {
-      type: String,
-      required: true
-    },
-    title: {
-      type: String
-    },
-    first_name: {
-      type: String
-    },
-    last_name: {
-      type: String
-    },
-    email: {
-      type: String
-    },
-    phone: {
-      type: String
-    },
-    gender: {
-      type: String
-    },
-    image: {
-      type: String
-    },
-    status: {
-      type: String
-    },
-  },
 
-  setup () {
+  async setup () {
+    const attendance = useAttendanceService()
+    const attendances = ref([])
+    const $q = useQuasar()
+
+    const visibleColumns = computed(() => {
+      return $q.screen.gt.xs
+        ? ['image', 'title', 'first_name', 'last_name', 'phone', 'email', 'status', 'view']
+        : ['image', 'title', 'first_name', 'view']
+    })
+
+    attendances.value = await attendance.list()
     return {
-      columns
+      columns,
+      rows,
+      visibleColumns
     }
   }
 })
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -22,10 +22,10 @@
       bordered
       class="bg-grey-1"
     >
-      <div class="tw-mt-4 tw-text-lg text-purple tw-text-center">Attendance System</div>
+      <div class="tw-mt-4 tw-text-lg tw-text-gray-600 tw-text-center">Attendance System</div>
       <q-list class="q-pt-md">
         <q-expansion-item
-          class="tw-text-sm sm:tw-text-base text-purple"
+          class="tw-text-sm sm:tw-text-base tw-text-gray-600"
           v-for="link in links"
           :key="link.title"
           :icon="link.icon"
@@ -50,50 +50,24 @@
           v-ripple
           clickable
         >
-          <q-item-section class="text-purple" avatar>
+          <q-item-section class="tw-text-gray-600" avatar>
             <q-icon name="logout" />
           </q-item-section>
-          <q-item-section class="tw-text-sm sm:tw-text-base text-purple">Logout</q-item-section>
+          <q-item-section class="tw-text-sm sm:tw-text-base tw-text-gray-600">Logout</q-item-section>
         </q-item>
       </q-list>
     </q-drawer>  
     <q-page-container>
-      <div v-if="attendances" class="tw-mt-8 md:tw-mt-0">
-          <div v-for="attendance in attendances" :key="attendance.id" class="tw-my-2 md:tw-my-10 tw-mx-4 md:tw-w-3/5 md:tw-mx-auto">
-            <div class="tw-flex tw-justify-between tw-border-b-2 twtext-purple">
-              <div class="tw-flex tw-justify-start">
-                <q-item-section avatar>
-                  <img class="tw-w-16 tw-object-cover tw-h-16 tw-rounded-full" :src="attendance.image" >
-                  <div class="tw-w-4 tw-h-4 tw-border-green-600 tw-rounded-full"></div>
-                </q-item-section>
-                <div class="tw-mt-2">
-                  <div class="tw-text-sm md:tw-text-lg text-purple tw-font-serif">
-                    {{ attendance.title }}
-                  </div>
-                  <div class="md:tw-text-base text-purple tw-font-mono">
-                    {{ attendance.first_name }} {{ attendance.last_name }}
-                    <p class="text-purple tw-mb-4 tw-text-xs">{{ attendance.status }} at 12:45 am</p>                
-                  </div>
-                </div>
-              </div>
-              <div class="tw-mt-5">
-                <q-badge class="tw-py-1 tw-px-2 tw-right-0" v-if="attendance.status === 'admitted'" color="positive" label="Approved" />
-                <q-badge class="tw-py-1 tw-px-2 tw-right-0" v-if="attendance.status === 'cancel'" color="negative" label="Cancelled" />
-                <q-badge class="tw-py-1 tw-px-2 tw-right-0" v-if="attendance.status == 'pending' " color="warning" label="Pending" /> 
-              </div>
-            </div>
+      <suspense>
+        <template #default>
+          <visitors-list></visitors-list>
+        </template>
+        <template #fallback>
+          <div>
+            Loading...
           </div>
-          <div class="q-pa-lg flex tw--mt-6 md:tw--mt-0 flex-center">
-            <q-pagination
-              v-model="attendances"
-              :max="5"
-              direction-links
-            />
-          </div>
-      </div>
-      <visitors-list
-        
-      ></visitors-list>
+        </template>
+      </suspense>      
       <router-view />
     </q-page-container>
   </q-layout>
@@ -104,86 +78,6 @@ import { defineComponent, ref } from 'vue';
 import { useAttendanceService } from '../composables/attendanceService';
 import VisitorsList from '../components/VisitorsList.vue'
 
-const attendanceList = [
-  {
-    id: "1",    
-    title: "Senator",
-    first_name: "Musa",    
-    last_name: "Muhammad",    
-    email: "musamohd@gmail.com",
-    phone: "08109342356",
-    gender: "male",
-    image: "/img/pexels-public-domain-pictures-42059.jpg",
-    status: 'admitted'
-  },
-  {
-    id: "2",    
-    title: "Mr",
-    first_name: "Nasir",    
-    last_name: "Lawal",    
-    email: "nla@gmail.com",
-    phone: "08109342356",
-    gender: "male",
-    image: "/img/pexels-polina-tankilevitch-4518599.jpg",
-    status: "pending"
-  },
-  {
-    id: "3",    
-    title: "Governor",
-    first_name: "Ibrahim",    
-    last_name: "Muazu",    
-    email: "ibrahimmuaz@gmail.com",
-    phone: "08109342356",
-    gender: "male",
-    image: "/img/pexels-naman-nayar-922764.jpg",
-    status: 'admitted'
-  },
-  {
-    id: "4",    
-    title: "Chairman",
-    first_name: "Isah",    
-    last_name: "Ahmad",    
-    email: "isah2312@gmail.com",
-    phone: "08109342356",
-    gender: "male",
-    image: "/img/pexels-ready-made-3850652.jpg",
-    status: 'cancel'
-  },
-  {
-    id: "5",    
-    title: "Rep",
-    first_name: "Muhammada",    
-    last_name: "Junaid",    
-    email: "mohdjunnaid@gmail.com",
-    phone: "08109342356",
-    gender: "male",
-    image: "/img/pexels-pixabay-161559.jpg",
-    status: 'cancel'
-  },
-  {
-    id: "6",    
-    title: "Mrs",
-    first_name: "Farida",    
-    last_name: "Aliyu",    
-    email: "farido@gmail.com",
-    phone: "08109342356",
-    gender: "female",
-    image: "/img/pexels-wendy-wei-1656666.jpg",
-    status: "pending"
-  },
-  {
-    id: "7",    
-    title: "Developer",
-    first_name: "Nasir",    
-    last_name: "Ibrahim",    
-    email: "nasirib@gmail.com",
-    phone: "08109342356",
-    gender: "male",
-    image: "/img/pexels-julia-volk-5273044.jpg",
-    status: 'admitted'
-  }
-]
-
 const linksData = [
   {
     title: 'Manage Visitors',
@@ -191,7 +85,7 @@ const linksData = [
     children: [
       {
         title: 'New Visitor',
-        icon: 'house',
+        icon: 'account_balance',
         to: { name: 'new-visitor' }
       },
       {
@@ -216,19 +110,12 @@ export default defineComponent({
 
   setup () {
     const leftDrawerOpen = ref(false);
-    const attendance = useAttendanceService()
-    const attendances = ref(null)
-
-    attendances.value = attendance.list()
-    console.log(attendances.value);
     const toggleLeftDrawer = () => { leftDrawerOpen.value = !leftDrawerOpen.value }
 
     return {
       links: linksData,
       leftDrawerOpen,
-      toggleLeftDrawer,
-      attendances: attendanceList
-      
+      toggleLeftDrawer      
     }
   }
 })
