2407e571a90171ee5980d2b31d04e220762e67c2
Nasir <nasirlawal001@gmail.com>
2021-07-18 11:57:11 +0100

    add getAvartarColor to the visitor details component

--- src/components/VisitorDetails.vue
+++ src/components/VisitorDetails.vue
@@ -2,10 +2,19 @@
   <transition appear
     enter-active-class="animated slideInLeft"
     leave-active-class="animated slideOutRight">
-    <div class="q-pa-md q-mt-lg tw-w-full">
-      <div class="tw--ml-3 sm:tw--ml-2 tw-text-center tw-font-thin tw-font-mono tw-w-12 tw-h-12 tw-overflow-hidden tw-shadow-lg md:tw-w-32 md:tw-h-32 tw-object-cover tw-rounded-full hover:tw-shadow-md" :style="{'background-color': getAvatarBackgroundColor(visitor.first_name)}">
-        <p class="tw-text-2xl tw-pt-2 sm:tw-pt-12 tw-text-gray-300">{{ visitor.first_name[0].toUpperCase() }}{{ visitor.last_name[0].toUpperCase() }}</p>
+    <div class="q-pa-md q-mt-lg tw-mx-auto tw-w-1/4">
+      <div class="tw--ml-3 sm:tw--ml-2 
+        tw-text-center tw-font-mono tw-w-12 tw-h-12 tw-shadow-lg 
+        md:tw-w-32 md:tw-h-32 tw-rounded-full hover:tw-shadow-md" 
+        :style="{'background-color': getAvatarBackgroundColor(visitor.first_name)}">
+        <p class="tw-text-2xl tw-pt-2 sm:tw-pt-12 tw-text-gray-300">
+          {{ visitor.first_name[0].toUpperCase() }}{{ visitor.last_name[0].toUpperCase() }}
+        </p>
       </div>
+      <div v-if="visitor.status === 'admitted'" class="bg-positive tw--mt-7 tw-border-2 tw-h-5 tw-w-5 tw-rounded-full"></div>
+      <div v-if="visitor.status === 'cancel'" class="bg-negative tw--mt-7 tw-border-2 tw-h-5 tw-w-5 tw-rounded-full"></div>
+      <div v-if="visitor.status === 'pending'" class="bg-warning tw--mt-7 tw-border-2 tw-h-5 tw-w-5 tw-rounded-full"></div>
+      <div v-if="visitor.status === 'finish'" class="bg-primary tw--mt-7 tw-border-2 tw-h-5 tw-w-5 tw-rounded-full"></div>
     </div>
   </transition>
 </template>
@@ -21,7 +30,7 @@ export default defineComponent({
   async setup() {
     const route = useRoute()
     const data = useAttendanceService()
-    const visitor = ref({})
+    const visitor = ref(null)
 
     const colors = [
       '#1abc9c',
