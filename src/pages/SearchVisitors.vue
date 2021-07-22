4ad2bed6a107c50cde9677a31fccec73aecbae41
Nasir <nasirlawal001@gmail.com>
2021-07-22 23:39:13 +0100

    skeletal to the search visitor page

--- src/pages/SearchVisitors.vue
+++ src/pages/SearchVisitors.vue
@@ -4,7 +4,7 @@
     enter-active-class="animated slideInLeft"
     leave-active-class="animated slideOutRight"
   >
-    <div class="q-pa-md q-mt-lg tw-w-full xl:tw-w-5/6 tw-mx-auto">
+    <div v-if="rows.length" class="q-pa-md q-mt-lg tw-w-full xl:tw-w-5/6 tw-mx-auto">
       <q-btn 
         dense
         icon="filter_list"
@@ -86,6 +86,62 @@
           </q-tr>
         </template>
       </q-table>
+    </div>
+    <div v-else class="q-pa-md q-mt-lg tw-w-full xl:tw-w-5/6 tw-h-5/6 tw-mx-auto">
+      <q-markup-table>
+        <thead>
+          <tr>
+            <th class="text-left">
+              <q-skeleton animation="blink" type="text" />
+            </th>
+            <th class="text-right">
+              <q-skeleton animation="blink" type="text" />
+            </th>
+            <th class="text-right">
+              <q-skeleton animation="blink" type="text" />
+            </th>
+            <th class="text-right">
+              <q-skeleton animation="blink" type="text" />
+            </th>
+            <th class="text-right">
+              <q-skeleton animation="blink" type="text" />
+            </th>
+            <th class="text-right">
+              <q-skeleton animation="blink" type="text" />
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="n in 6" :key="n">
+            <td class="text-left">
+              <q-skeleton animation="blink" type="QAvatar" />
+            </td>
+            <td class="text-right">
+              <q-skeleton animation="blink" type="text" width="50px" />
+            </td>
+            <td class="text-right">
+              <q-skeleton animation="blink" type="text" width="50px" />
+            </td>
+            <td class="text-right">
+              <q-skeleton animation="blink" type="text" width="65px" />
+            </td>
+            <td class="text-right">
+              <q-skeleton animation="blink" type="text" width="40px" />
+            </td>
+            <td class="text-right">
+              <q-skeleton animation="blink" type="text" width="85px" />
+            </td>
+          </tr>
+        </tbody>
+      </q-markup-table>
+      <q-btn
+        round
+        class="tw-float-right tw-mt-4 tw-bottom-0"
+        color="primary"
+        size="16px"
+        icon="add"
+        to="new-visitor"
+      />
     </div>
   </transition>
 </template>
