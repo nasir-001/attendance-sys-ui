90186618d3f1c0f0a6c321d14c31de643532b39c
Nasir <nasirlawal001@gmail.com>
2021-07-23 15:07:26 +0100

    add trigger notification function

--- src/composables/attendanceService.js
+++ src/composables/attendanceService.js
@@ -5,28 +5,27 @@ import { useRouter } from 'vue-router';
 export function useAttendanceService() {
   const $q = useQuasar()
   const router = useRouter()
+  const trigerNotification = (type, message, icon) => {
+    $q.notify({
+      type: type,
+      message: message,
+      position: 'top',
+      icon: icon,
+      timeout: 2000
+    })
+  }
   return {
     async list() {
       const response = await api.get(`/api/attendance`)
         .catch(() => {
-          $q.notify({
-            color: 'negative',
-            position: 'top',
-            message: 'Failed to load visitors',
-            icon: 'report_problem'
-          })
+          trigerNotification('negative', 'Failed to load visitors', 'report_problem')
         })
       return response.data;
     },
     async attendance(id) {
       const response = await api.get(`/api/attendance/${id}`)
         .catch(() => {
-          $q.notify({
-            color: 'negative',
-            position: 'top',
-            message: 'Failed to load visitors',
-            icon: 'report_problem'
-          })
+          trigerNotification('negative', 'Failed to view visitor', 'report_problem')
         })
       return response.data;
     },
@@ -34,155 +33,73 @@ export function useAttendanceService() {
       console.log(id);
       const response = await api.patch(`/api/attendance/${id}`, payload)
       .then(() => {
-        $q.notify({
-          color: 'positive',
-          position: 'top',
-          message: 'Visitor has been editted successfully',
-          icon: 'check_circle'
-        })
+        trigerNotification('positive', 'Visitor has been editted successfully', 'check_circle')
       })
       .then(() => {
         router.push({ name: 'dashborad' })
       })
       .catch(() => {
-        $q.notify({
-          color: 'negative',
-          position: 'top',
-          message: 'Failed to edit visitor',
-          icon: 'report_problem'
-        })
+        trigerNotification('negative', 'Failed to edit visitor', 'report_problem')
       })
     },
     async admitVisitor(id, payload) {
       console.log(id);
       const response = await api.patch(`/api/attendance/${id}`, payload)
       .then(() => {
-        $q.notify({
-          color: 'positive',
-          position: 'top',
-          message: 'Visitor has been admitted',
-          icon: 'check_circle'
-        })
+        trigerNotification('positive', 'Visitor has been admitted', 'check_circle')
       })
       .then(() => {
         router.push({ name: 'dashborad' })
       })
       .catch(() => {
-        $q.notify({
-          color: 'negative',
-          position: 'top',
-          message: 'Failed to admit visitor',
-          icon: 'report_problem'
-        })
+        trigerNotification('negative', 'Failed to admit visitor', 'report_problem')
       })
     },
     async cancelVisitor(id, payload) {
       console.log(id);
       const response = await api.patch(`/api/attendance/${id}`, payload)
       .then(() => {
-        $q.notify({
-          color: 'positive',
-          position: 'top',
-          message: 'Visitor has been admitted',
-          icon: 'check_circle'
-        })
+        trigerNotification('positive', 'Visitor has been admitted', 'check_circle')
       })
       .then(() => {
         router.push({ name: 'dashborad' })
       })
       .catch(() => {
-        $q.notify({
-          color: 'negative',
-          position: 'top',
-          message: 'Failed to admit visitor',
-          icon: 'report_problem'
-        })
+        trigerNotification('negative', 'Failed to admit visitor', 'report_problem')
       })
     },
     async visitorDepart(id, payload) {
       console.log(id);
       const response = await api.patch(`/api/attendance/${id}`, payload)
       .then(() => {
-        $q.notify({
-          color: 'positive',
-          position: 'top',
-          message: 'Visitor has been departed',
-          icon: 'check_circle'
-        })
+        trigerNotification('positive', 'Visitor has been departed', 'check_circle')
       })
       .then(() => {
         router.push({ name: 'dashborad' })
       })
       .catch(() => {
-        $q.notify({
-          color: 'negative',
-          position: 'top',
-          message: 'Failed to depart visitor',
-          icon: 'report_problem'
-        })
+        trigerNotification('negative', 'Failed to depart visitor', 'report_problem')
       })
     },
     async newVisitor(payload) {
       const response = await api.post(`/api/attendance/`, payload)
       .then(() => {
-        $q.notify({
-          color: 'positive',
-          position: 'top',
-          message: 'Visitor was successfully added',
-          icon: 'check_circle'
-        })
+        trigerNotification('positive', 'Visitor was successfully added', 'check_circle')
       })
       .then(() => {
         router.push({ name: 'dashborad' })
       })
       .catch(() => {
-        $q.notify({
-          color: 'negative',
-          position: 'top',
-          message: 'Failed to edit visitor',
-          icon: 'report_problem'
-        })
-      })
-    },
-    async deleteVisitor(id) {
-      const response = await api.delete(`/api/attendance/${id}`)
-      .then(() => {
-        $q.notify({
-          color: 'positive',
-          position: 'top',
-          message: 'Visitor was successfully deleted',
-          icon: 'check_circle'
-        })
-      })
-      .then(() => {
-        router.push({ name: 'dashborad' })
-      })
-      .catch(() => {
-        $q.notify({
-          color: 'negative',
-          position: 'top',
-          message: 'Failed to delete visitor',
-          icon: 'report_problem'
-        })
+        trigerNotification('negative', 'Failed to edit visitor', 'report_problem')
       })
     },
     async filterVisitorByName(name) {
       const response = await api.get(`/api/attendance/?first_name=${name}`)
       .then(() => {
-        $q.notify({
-          color: 'positive',
-          position: 'top',
-          message: 'Visitor with that name is found',
-          icon: 'done_all'
-        })
+        trigerNotification('positive', "Found match visitor's name", 'done_all')
       })
       .catch(() => {
-        $q.notify({
-          color: 'negative',
-          position: 'top',
-          message: 'Failed to delete visitor',
-          icon: 'report_problem'
-        })
+        trigerNotification('negative', "Couldn't find visitor with that name", 'report_problem')
       })
       return response.data
     }
--- src/pages/SearchVisitors.vue
+++ src/pages/SearchVisitors.vue
@@ -78,10 +78,10 @@
               {{ props.row.email }}
             </q-td>
             <q-td key="status" :props="props">
-              <q-badge class="tw-py-1.5 tw-px-2 tw-right-0 tw-uppercase" v-if="props.row.visit.status === 'admitted'" color="positive" label="Admitted" />
-              <q-badge class="tw-py-1.5 tw-px-2 tw-right-0 tw-uppercase" v-if="props.row.visit.status === 'cancelled'" color="negative" label="Cancelled" />
-              <q-badge class="tw-py-1.5 tw-px-2 tw-right-0 tw-uppercase" v-if="props.row.visit.status == 'pending' " color="warning" label="Pending" /> 
-              <q-badge class="tw-py-1.5 tw-px-2 tw-right-0 tw-uppercase" v-if="props.row.visit.status == 'finished' " color="primary" label="Departed" /> 
+              <q-badge class="tw-py-1 tw-px-1 sm:tw-py-1.5 sm:tw-px-2 tw-right-0 tw-uppercase" v-if="props.row.visit.status === 'admitted'" color="positive" label="Admitted" />
+              <q-badge class="tw-py-1 tw-px-1 sm:tw-py-1.5 sm:tw-px-2 tw-right-0 tw-uppercase" v-if="props.row.visit.status === 'cancelled'" color="negative" label="Cancelled" />
+              <q-badge class="tw-py-1 tw-px-1 sm:tw-py-1.5 sm:tw-px-2 tw-right-0 tw-uppercase" v-if="props.row.visit.status == 'pending' " color="warning" label="Pending" /> 
+              <q-badge class="tw-py-1 tw-px-1 sm:tw-py-1.5 sm:tw-px-2 tw-right-0 tw-uppercase" v-if="props.row.visit.status == 'finished' " color="primary" label="Departed" /> 
             </q-td>
             <q-td key="view" :props="props" class="tw-mr-4">
               <q-btn :to="{ name: 'visitor-details', params: { id: props.row.id } }" label="View" dense color="primary" class="tw-text-xs tw-py-2 tw-px-3 tw--mr-2" />
