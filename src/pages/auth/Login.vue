50d4a2b1130aea5d7e63b64c071474cda4c4c477
Nasir <nasirlawal001@gmail.com>
2021-08-14 17:28:01 +0100

    style(login): add transition to the login page

--- src/pages/auth/Login.vue
+++ src/pages/auth/Login.vue
@@ -1,57 +1,60 @@
 <template>
-  <div class="q-pa-sm row justify-center">
-    <div class="col-12 col-sm-7 col-md-4 col-xl-3 tw-rounded-lg tw-border-2 tw-text-gray-400 tw-mt-20">
-      <q-card flat class="q-pb-sm w100">
-        <form @submit.prevent="loginUser()">
-          <q-card-section class="text-center q-my-md">
-            <div class="text-h5">Login your account.</div>
-          </q-card-section>
-          <q-card-section>
-            <q-input
-              outlined
-              auto-focus
-              lazy-rules
-              type="email"
-              label="email"
-              v-model="email"
-              :rules="[
-                val => !!val || 'Field is required',
-                val => emailValidator(val)
-              ]"
-            />
-          </q-card-section>
-          <q-card-section class="tw--mt-2">
-            <q-input
-              class="sm:tw--mt-0 tw-overflow-hidden"
-              outlined
-              lazy-rules
-              label="Password"
-              v-model="password"
-              :type="isPwd ? 'password' : 'text'"
-              :rules="[val => !!val || 'Field is required']"
-            >
-              <template v-slot:append>
-                <q-icon
-                  :name="isPwd ? 'visibility_off' : 'visibility'"
-                  class="cursor-pointer tw-text-gray-400"
-                  @click="isPwd = !isPwd"
-                />
-              </template>
-            </q-input>
-          </q-card-section>
-          <q-card-section class="tw--mt-2 tw-mb-6">
-            <q-btn
-              label="Login"
-              type="submit"
-              color="primary"
-              :disabled="emptyRequiredField || loginBtnIsLoading"
-              class="full-width tw-text-base sm:tw-text-lg tw-rounded-full sm:tw-rounded-lg tw--mt-6 sm:tw--mt-0"
-            />
-          </q-card-section>
-        </form>
-      </q-card>
+  <transition appear
+    enter-active-class="animated slideInUp">
+    <div class="q-pa-sm row justify-center">
+      <div class="col-12 col-sm-7 col-md-4 col-xl-3 tw-rounded-lg tw-border-2 tw-text-gray-400 tw-mt-20">
+        <q-card flat class="q-pb-sm w100">
+          <form @submit.prevent="loginUser()">
+            <q-card-section class="text-center q-my-md">
+              <div class="text-h5">Login your account.</div>
+            </q-card-section>
+            <q-card-section>
+              <q-input
+                outlined
+                auto-focus
+                lazy-rules
+                type="email"
+                label="email"
+                v-model="email"
+                :rules="[
+                  val => !!val || 'Field is required',
+                  val => emailValidator(val)
+                ]"
+              />
+            </q-card-section>
+            <q-card-section class="tw--mt-2">
+              <q-input
+                class="sm:tw--mt-0 tw-overflow-hidden"
+                outlined
+                lazy-rules
+                label="Password"
+                v-model="password"
+                :type="isPwd ? 'password' : 'text'"
+                :rules="[val => !!val || 'Field is required']"
+              >
+                <template v-slot:append>
+                  <q-icon
+                    :name="isPwd ? 'visibility_off' : 'visibility'"
+                    class="cursor-pointer tw-text-gray-400"
+                    @click="isPwd = !isPwd"
+                  />
+                </template>
+              </q-input>
+            </q-card-section>
+            <q-card-section class="tw--mt-2 tw-mb-6">
+              <q-btn
+                label="Login"
+                type="submit"
+                color="primary"
+                :disabled="emptyRequiredField || loginBtnIsLoading"
+                class="full-width tw-text-base sm:tw-text-lg tw-rounded-full sm:tw-rounded-lg tw--mt-6 sm:tw--mt-0"
+              />
+            </q-card-section>
+          </form>
+        </q-card>
+      </div>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script>
