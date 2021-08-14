9888faec7116690eca61aa18c393770cabc17e42
Nasir <nasirlawal001@gmail.com>
2021-08-14 11:58:39 +0100

    refactor(visitor detail): implementation of swapping models

--- src/pages/views/VisitorDetails.vue
+++ src/pages/views/VisitorDetails.vue
@@ -8,47 +8,47 @@
         <!-- visitor detail -->
         <q-card class="tw-pt-5 tw-rounded-3xl tw-shadow-xl hover:tw-shadow-2xl">
           <q-card-section class="text-grey-7 tw-font-mono">
-            {{ formatedDate(visitor.visit.date) }}
+            {{ formatedDate(visit.date) }}
           </q-card-section>
           <div class="tw-flex tw-pt-5 tw-ml-8 sm:tw-ml-12 tw-justify-center">
             <div class="sm:tw--ml-2 tw-text-center tw-font-mono tw-w-20 
               tw-h-20 tw-shadow-lg md:tw-w-32 md:tw-h-32 tw-rounded-full hover:tw-shadow-md" 
-              :style="{'background-color': getAvatarBackgroundColor(visitor.first_name)}">
+              :style="{'background-color': getAvatarBackgroundColor(visit.first_name)}">
               <p class="tw-text-2xl tw-pt-6 md:tw-pt-12 tw-text-gray-300">
-                {{ visitor.first_name[0].toUpperCase() }}{{ visitor.last_name[0].toUpperCase() }}
+                {{ visit.first_name[0].toUpperCase() }}{{ visit.last_name[0].toUpperCase() }}
               </p>
             </div>
             <div class="tw--ml-6">
-              <div v-if="visitor.visit.status === 'admitted' " class="bg-positive tw-mt-16 md:tw-mt-24 tw-border-white tw-border-2 tw-w-4 tw-h-4 md:tw-h-5 md:tw-w-5 tw-rounded-full"></div>
-              <div v-if="visitor.visit.status === 'cancelled' " class="bg-negative tw-mt-16 md:tw-mt-24 tw-border-white tw-border-2 tw-w-4 tw-h-4 md:tw-h-5 md:tw-w-5 tw-rounded-full"></div>
-              <div v-if="visitor.visit.status === 'pending' " class="bg-warning tw-mt-16 md:tw-mt-24 tw-border-white tw-border-2 tw-w-4 tw-h-4 md:tw-h-5 md:tw-w-5 tw-rounded-full"></div>
-              <div v-if="visitor.visit.status === 'finished' " class="bg-primary tw-mt-16 md:tw-mt-24 tw-border-white tw-border-2 tw-w-4 tw-h-4 md:tw-h-5 md:tw-w-5 tw-rounded-full"></div>
+              <div v-if="visit.visit.status === 'admitted' " class="bg-positive tw-mt-16 md:tw-mt-24 tw-border-white tw-border-2 tw-w-4 tw-h-4 md:tw-h-5 md:tw-w-5 tw-rounded-full"></div>
+              <div v-if="visit.visit.status === 'cancelled' " class="bg-negative tw-mt-16 md:tw-mt-24 tw-border-white tw-border-2 tw-w-4 tw-h-4 md:tw-h-5 md:tw-w-5 tw-rounded-full"></div>
+              <div v-if="visit.visit.status === 'pending' " class="bg-warning tw-mt-16 md:tw-mt-24 tw-border-white tw-border-2 tw-w-4 tw-h-4 md:tw-h-5 md:tw-w-5 tw-rounded-full"></div>
+              <div v-if="visit.visit.status === 'finished' " class="bg-primary tw-mt-16 md:tw-mt-24 tw-border-white tw-border-2 tw-w-4 tw-h-4 md:tw-h-5 md:tw-w-5 tw-rounded-full"></div>
             </div>
             <div class="tw-mt-16 md:tw-mt-24 tw-ml-2">
-              <q-btn @click="showEditVisitor = true" flat dense color="primary" icon="edit" />
+              <q-btn @click="showEditVisit = true" flat dense color="primary" icon="edit" />
             </div>
           </div>
           <div>
             <div class="tw-text-center tw-text-gray-600 md:tw-pt-2 tw-text-lg md:tw-text-xl tw-font-semibold tw-uppercase">
-              {{ visitor.title }}
+              {{ visit.title }}
             </div>
             <div class="tw-text-center tw-text-gray-500 tw-text-base md:tw-text-lg tw-font-semibold tw-pt-1 tw-uppercase tw-font-mono tw-tracking-wide">
-              {{ visitor.first_name }} {{ visitor.last_name }}
+              {{ visit.first_name }} {{ visit.last_name }}
             </div>
-            <div v-if="visitor.email" class="tw-text-center tw-text-gray-400 tw-text-md tw-font-semibold tw-pt-1 tw-font-mono tw-tracking-wide">
-              <q-icon name="mail" /> {{ visitor.email }} 
+            <div v-if="visit.email" class="tw-text-center tw-text-gray-400 tw-text-md tw-font-semibold tw-pt-1 tw-font-mono tw-tracking-wide">
+              <q-icon name="mail" /> {{ visit.email }} 
             </div>
-            <div v-if="visitor.visit.status === 'admitted' " class="row justify-center q-mt-sm">
+            <div v-if="visit.visit.status === 'admitted' " class="row justify-center q-mt-sm">
               <div class="text-green-7 tw-font-mono">
                 <q-badge color="primary">
-                  {{ visitor.visit.admitted_time }}
+                  {{ visit.visit.admitted_time }}
                 </q-badge>
               </div>
             </div>
-            <div v-if="visitor.visit.status === 'finished' " class="row justify-center q-mt-sm">
+            <div v-if="visit.visit.status === 'finished' " class="row justify-center q-mt-sm">
               <div class="text-green-7 tw-font-semibold q-mx-sm tw-font-mono">
                 <q-badge color="primary">
-                  {{ visitor.visit.admitted_time }}
+                  {{ visit.visit.admitted_time }}
                 </q-badge>
               </div>
               <div class="text-grey-7">
@@ -56,7 +56,7 @@
               </div>
               <div class="text-blue-7 tw-font-semibold q-mx-sm tw-font-mono">
                 <q-badge color="primary">
-                  {{ visitor.visit.depart_time }}
+                  {{ visit.visit.depart_time }}
                 </q-badge>
               </div>
             </div>
@@ -65,7 +65,7 @@
             <q-card-actions class="">
               <q-form @submit.prevent="admitVisitor">
                 <q-btn 
-                  v-if="visitor.visit.status === 'pending' "
+                  v-if="visit.visit.status === 'pending' "
                   type="submit"
                   color="primary" 
                   class="q-mx-sm tw-w-32 tw-font-semibold" 
@@ -79,7 +79,7 @@
             <q-card-actions class="">
               <q-form @submit.prevent="cancelVisitor">
                 <q-btn 
-                  v-if="visitor.visit.status === 'pending' "
+                  v-if="visit.visit.status === 'pending' "
                   type="submit"
                   color="negative" 
                   class="q-mx-sm tw-w-32 tw-font-semibold" 
@@ -94,7 +94,7 @@
           <q-card-actions class="tw--mt-20 tw-pb-8">
             <q-form class="tw-mx-auto" @submit.prevent="visitorLeave">
               <q-btn 
-                v-if="visitor.visit.status === 'admitted' " 
+                v-if="visit.visit.status === 'admitted' " 
                 type="submit"
                 color="primary" 
                 class="q-mx-sm tw-w-42 tw-font-semibold tw-mx-4" 
@@ -122,7 +122,7 @@
                   auto-focus
                   lazy-rules
                   type="text"
-                  v-model="visitor.title"
+                  v-model="visit.title"
                   label="Title"
                   :rules="[
                     val => !!val || 'Field is required']"
@@ -134,7 +134,7 @@
                   auto-focus
                   lazy-rules
                   type="text"
-                  v-model="visitor.first_name"
+                  v-model="visit.first_name"
                   label="First Name"
                   :rules="[
                     val => !!val || 'Field is required']"
@@ -146,7 +146,7 @@
                   auto-focus
                   lazy-rules
                   type="text"
-                  v-model="visitor.last_name"
+                  v-model="visit.last_name"
                   label="Last Name"
                   :rules="[
                     val => !!val || 'Field is required']"
@@ -158,7 +158,7 @@
                   auto-focus
                   lazy-rules
                   type="text"
-                  v-model="visitor.email"
+                  v-model="visit.email"
                   label="Email"
                   :rules="[
                     val => !!val || 'Field is required',
@@ -172,7 +172,7 @@
                   auto-focus
                   lazy-rules
                   type="number"
-                  v-model="visitor.phone"
+                  v-model="visit.phone"
                   label="Phone"
                   :rules="[
                     val => !!val || 'Field is required',
@@ -183,7 +183,7 @@
               <q-card-section class="q-pt-none">
               <q-select 
                 outlined 
-                v-model="visitor.gender"
+                v-model="visit.gender"
                 :options="gender" 
                 label="Gender"
               />
@@ -191,17 +191,17 @@
               <q-card-section class="q-pt-md">
                 <q-select 
                   outlined 
-                  v-model="visitor.visit.status" 
+                  v-model="visit.visit.status" 
                   :options="options" 
                   label="Status"
                 />
               </q-card-section>
-              <q-card-section v-if="visitor.visit.status === 'admitted'" class="q-pt-md">
-                <q-input label="Admitted time" outlined v-model="visitor.visit.admitted_time" mask="time" :rules="['time']">
+              <q-card-section v-if="visit.visit.status === 'admitted'" class="q-pt-md">
+                <q-input label="Admitted time" outlined v-model="visit.visit.admitted_time" mask="time" :rules="['time']">
                   <template v-slot:append>
                     <q-icon name="access_time" class="cursor-pointer">
                       <q-popup-proxy transition-show="scale" transition-hide="scale">
-                        <q-time now-btn flat v-model="visitor.visit.admitted_time">
+                        <q-time now-btn flat v-model="visit.visit.admitted_time">
                           <div class="row items-center justify-end">
                             <q-btn v-close-popup label="Save" color="danger" flat />
                           </div>
@@ -211,13 +211,13 @@
                   </template>
                 </q-input>
               </q-card-section>
-              <q-card-section v-if="visitor.visit.status === 'finished'" class="q-pt-md">
+              <q-card-section v-if="visit.visit.status === 'finished'" class="q-pt-md">
                 <div class="tw-flex tw-justify-end tw--mb-4">
-                  <q-input label="Admitted time" outlined class="tw-w-full tw-mr-1" v-model="visitor.visit.admitted_time" mask="time" :rules="['time']">
+                  <q-input label="Admitted time" outlined class="tw-w-full tw-mr-1" v-model="visit.visit.admitted_time" mask="time" :rules="['time']">
                     <template v-slot:append>
                       <q-icon name="access_time" class="cursor-pointer">
                         <q-popup-proxy transition-show="scale" transition-hide="scale">
-                          <q-time now-btn flat v-model="visitor.visit.admitted_time">
+                          <q-time now-btn flat v-model="visit.visit.admitted_time">
                             <div class="row items-center justify-end">
                               <q-btn v-close-popup label="Save" color="danger" flat />
                             </div>
@@ -226,11 +226,11 @@
                       </q-icon>
                     </template>
                   </q-input>
-                  <q-input label="Depart time" outlined class="tw-w-full tw-ml-1" v-model="visitor.visit.depart_time" mask="time" :rules="['time']">
+                  <q-input label="Depart time" outlined class="tw-w-full tw-ml-1" v-model="visit.visit.depart_time" mask="time" :rules="['time']">
                     <template v-slot:append>
                       <q-icon name="access_time" class="cursor-pointer">
                         <q-popup-proxy transition-show="scale" transition-hide="scale">
-                          <q-time now-btn flat v-model="visitor.visit.depart_time">
+                          <q-time now-btn flat v-model="visit.visit.depart_time">
                             <div class="row items-center justify-end">
                               <q-btn v-close-popup label="Save" color="danger" flat />
                             </div>
@@ -242,11 +242,11 @@
                 </div>
               </q-card-section>
               <q-card-section class="q-pt-md">
-                <q-input label="Arrival Date" outlined v-model="visitor.visit.date">
+                <q-input label="Arrival Date" outlined v-model="visit.visit.date">
                   <template v-slot:append>
                     <q-icon name="event" class="cursor-pointer">
                       <q-popup-proxy ref="qDateProxy" transition-show="scale" transition-hide="scale">
-                        <q-date v-model="visitor.visit.date">
+                        <q-date v-model="visit.visit.date">
                           <div class="row items-center justify-end">
                             <q-btn v-close-popup label="Save" color="primary" flat />
                           </div>
@@ -293,7 +293,7 @@ export default defineComponent({
   async setup() {
     const route = useRoute();
     const data = useAttendanceService();
-    const visitor = ref(null);
+    const visit = ref(null);
     const showEditVisitor = ref(false);
     const confirmDelete = ref(false);
     const editBtnIsLoading = ref(false);
@@ -313,33 +313,33 @@ export default defineComponent({
       return validatePhone(value)
     }
 
-    const editVisitorPayload = reactive({
-      title: '',
-      first_name: '',
-      last_name: '',
-      email: '',
-      phone: '',
-      gender: '',
-      visit: {
-        date: '',
-        status: '',
-        admitted_time: '',
-        depart_time: ''
+    const editVisitorPayload = reactive({      
+      date: '',
+      status: '',
+      admitted_time: '',
+      depart_time: '',
+      visitor: {
+        title: '',
+        first_name: '',
+        last_name: '',
+        email: '',
+        phone: '',
+        gender: ''
       }
     });
 
     function editVisitor() {
-      editBtnIsLoading.value = true;
-      editVisitorPayload.title = visitor.value.title,
-      editVisitorPayload.first_name = visitor.value.first_name,
-      editVisitorPayload.last_name = visitor.value.last_name,
-      editVisitorPayload.email = visitor.value.email,
-      editVisitorPayload.phone = visitor.value.phone,
-      editVisitorPayload.gender = visitor.value.gender,
-      editVisitorPayload.visit.date = visitor.value.visit.date,
-      editVisitorPayload.visit.status = visitor.value.visit.status,
-      editVisitorPayload.visit.admitted_time = visitor.value.visit.admitted_time,
-      editVisitorPayload.visit.depart_time = visitor.value.visit.depart_time
+      editBtnIsLoading.value = true;      
+      editVisitorPayload.date = visit.value.date,
+      editVisitorPayload.status = visit.value.status,
+      editVisitorPayload.admitted_time = visit.value.admitted_time,
+      editVisitorPayload.depart_time = visit.value.depart_time,
+      editVisitorPayload.visitor.title = visit.value.title,
+      editVisitorPayload.visitor.first_name = visit.value.first_name,
+      editVisitorPayload.visitor.last_name = visit.value.last_name,
+      editVisitorPayload.visitor.email = visit.value.email,
+      editVisitorPayload.visitor.phone = visit.value.phone,
+      editVisitorPayload.visitor.gender = visit.value.gender,
 
       api.patch(`/api/attendance/${route.params.id}`, editVisitorPayload)
       .then(() => {
@@ -369,17 +369,17 @@ export default defineComponent({
     function admitVisitor() {
       admitBtnIsLoading.value = true;
       const visitorAdmitPayload = reactive({
-        title: visitor.value.title,
-        first_name: visitor.value.first_name,
-        last_name: visitor.value.last_name,
-        email: visitor.value.email,
-        phone: visitor.value.phone,
-        gender: visitor.value.gender,
-        visit: {
-          date: visitor.value.visit.date,
-          status: admitted.value,
-          admitted_time: timeToReturn(),
-          depart_time: visitor.value.visit.depart_time
+        date: visit.value.date,
+        status: admitted.value,
+        admitted_time: timeToReturn(),
+        depart_time: visitor.value.visit.depart_time,
+        visitor: {
+          title: visit.value.visitor.title,
+          first_name: visit.value.visitor.first_name,
+          last_name: visit.value.visitor.last_name,
+          email: visit.value.visitor.email,
+          phone: visit.value.visitor.phone,
+          gender: visit.value.visitor.gender
         }
       })
 
@@ -409,15 +409,15 @@ export default defineComponent({
     function cancelVisitor() {
       cancelBtnIsLoading.value = true;
       const visitorCancelPayload = reactive({
-        title: visitor.value.title,
-        first_name: visitor.value.first_name,
-        last_name: visitor.value.last_name,
-        email: visitor.value.email,
-        phone: visitor.value.phone,
-        gender: visitor.value.gender,
-        visit: {
-          date: visitor.value.visit.date,
-          status: cancel.value
+        date: visit.value.date,
+        status: cancel.value    ,
+        visitor: {
+          title: visit.value.visitor.title,
+          first_name: visit.value.visitor.first_name,
+          last_name: visit.value.visitor.last_name,
+          email: visit.value.visitor.email,
+          phone: visit.value.visitor.phone,
+          gender: visit.value.visitor.gender,
         }
       })
 
@@ -447,17 +447,17 @@ export default defineComponent({
     function visitorLeave() {
       departBtnIsLoading.value = true;
       const visitorLeavePayload = reactive({
-        title: visitor.value.title,
-        first_name: visitor.value.first_name,
-        last_name: visitor.value.last_name,
-        email: visitor.value.email,
-        phone: visitor.value.phone,
-        gender: visitor.value.gender,
-        visit: {
-          date: visitor.value.visit.date,
-          status: finished.value,
-          admitted_time: visitor.value.visit.admitted_time,
-          depart_time: timeToReturn()
+        date: visit.value.date,
+        status: finished.value,
+        admitted_time: visit.value.admitted_time,
+        depart_time: timeToReturn(),
+        visitor: {
+          title: visit.value.visitor.title,
+          first_name: visit.value.visitor.first_name,
+          last_name: visit.value.visitor.last_name,
+          email: visit.value.visitor.email,
+          phone: visit.value.visitor.phone,
+          gender: visit.value.visitor.gender,
         }
       })
 
@@ -484,7 +484,7 @@ export default defineComponent({
         })
     };
 
-    visitor.value = await data.attendance(route.params.id);
+    visit.value = await data.attendance(route.params.id);
 
     return {
       options: [
@@ -493,7 +493,7 @@ export default defineComponent({
       gender: [
         'male', 'female'
       ],
-      visitor,
+      visit,
       getAvatarBackgroundColor,
       showEditVisitor,
       confirmDelete,
